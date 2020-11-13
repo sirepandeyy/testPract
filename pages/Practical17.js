2f14cd33931e4c01be27cbc2849e1f609fce1c93
Aman Kumar Pandey <44319659+sirepandeyy@users.noreply.github.com>
2020-11-14 00:24:24 +0530

    Update Practical17.js

--- pages/Practical17.js
+++ pages/Practical17.js
@@ -2,8 +2,6 @@ const time = document.getElementById('time'),
     greeting = document.getElementById('greeting'),
     name = document.getElementById('name'),
     focus = document.getElementById('focus');
-
-
 function showTime(){
     let today = new Date(),
     hour = today.getHours(),
